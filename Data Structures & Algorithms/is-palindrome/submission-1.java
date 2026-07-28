24ea38c5f36f4c90bdc0c0e193b14e0636128931
Rohit Kumar <85385695+rohitkumarvats@users.noreply.github.com>
2026-07-29 00:16:18 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // int left=0;
+        // int right=s.length()-1;
+
+        // while(left<right){
+        //     while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+        //         left++;
+        //     }
+
+        //     while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+        //         right--;
+        //     }
+
+        //     if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+        //         return false;
+        //     }
+
+        //     left++;
+        //     right--;
+        // }
+        // return true;
+
+        // Solution - 2
+        s=s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left=0;
+        int right=s.length()-1;
+
+        while(left<right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
