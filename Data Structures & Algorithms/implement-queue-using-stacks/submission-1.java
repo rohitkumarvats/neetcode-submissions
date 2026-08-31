113aa8c84acab56fa13783639e654189dc3c67b0
Rohit Kumar <85385695+rohitkumarvats@users.noreply.github.com>
2026-08-31 23:48:17 +0530

    Add: implement-queue-using-stacks - submission-1

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-1.java
@@ -0,0 +1,44 @@
+class MyQueue {
+    private final Deque<Integer> input;
+    private final Deque<Integer> output;
+
+    public MyQueue() {
+        input = new ArrayDeque<>();
+        output = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        shiftElements();
+        return output.pop();
+    }
+
+    public int peek() {
+        shiftElements();
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+
+    private void shiftElements() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
